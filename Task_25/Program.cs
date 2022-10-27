5a697730c7c812aa34b41f6db22ca01dab841056
Ilya12184 <volostnov.il@yandex.ru>
2022-10-27 11:47:46 +0300

    Добавлена задача 25

---
+++ Task_25/Program.cs
@@ -0,0 +1,13 @@
+﻿/*Напишите цикл, который принимает на вход два числа (A и B) 
+и возводит число A в натуральную степень B.*/
+Console.Write("Введите возводимое число: ");
+int A = int.Parse(Console.ReadLine()!);
+Console.Write("Введите степень для возведения: ");
+int B = int.Parse(Console.ReadLine()!);
+
+int multiplication = 1;
+for (int i = 0; i < B; i++)
+{
+    multiplication *= A;
+}
+Console.WriteLine(multiplication);
