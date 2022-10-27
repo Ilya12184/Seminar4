117acac98ff5555de0f30c96ea414352a0ff2b95
Ilya12184 <volostnov.il@yandex.ru>
2022-10-27 13:38:46 +0300

    Добавлена задача 27

---
+++ Task_27/Program.cs
@@ -0,0 +1,15 @@
+﻿/*Напишите программу, которая принимает на вход число 
+и выдаёт сумму цифр в числе.*/
+
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+
+int remains = 0;
+int sum = 0;
+for (int i = 0; number >= 1; i++)
+{
+    remains = number%10;
+    number = number/10;
+    sum += remains;
+}
+Console.WriteLine(sum);
